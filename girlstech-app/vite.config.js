3c71953138410a2f300e81cc83780b31e2750f89
Josyls <110506247+Josyls@users.noreply.github.com>
2025-11-25 20:41:11 -0300

    Update vite.config.js

--- girlstech-app/vite.config.js
+++ girlstech-app/vite.config.js
@@ -1,9 +1,8 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Define o caminho base para que o Vercel/GitHub Pages encontre os assets
   base: '/', 
-})
+});
